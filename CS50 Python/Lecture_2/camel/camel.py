1fa0254cd12058578903d653999f4f9f5110a219
allan.debiaggio <allan.de-biaggio@laplateforme.io>
2025-02-26 14:09:35 +0100

    Lecture 2 Exercise 1 Upload

---
+++ CS50 Python/Lecture_2/camel/camel.py
@@ -0,0 +1,35 @@
+"""
+Implement a program that :
+- Prompts the user for the name of a variable in camelCase
+- Outputs the corresponding name in snake_case
+"""
+
+def main():
+    
+    name = []
+    name += input("camelCase : ")
+    print(name)
+    print(snake_case(name))
+
+
+def snake_case(text):
+
+    # Creating a new list to store the text inputted and a count for the new index
+    new_name = []
+    new_name += text
+    count = 0
+
+    # Looping through every letter of the list
+    for _ in range(len(text)):
+
+        if text[_].isupper():
+            # Inserting the "_" in existing list, using count to adapt to its new length
+            new_name.insert(_ + count, "_")
+            count += 1
+
+    # Lowering letters in the final name
+    new_name = str(new_name).lower()
+    return new_name
+
+
+main()
