994bfe9d3632d4b01e27421b8f8e48ad7895fe0f
allan.debiaggio <allan.de-biaggio@laplateforme.io>
2025-03-01 21:59:06 +0100

    Lecture 2 Exercise 4 second draft Upload

--- CS50 Python/Lecture_2/plates/plates.py
+++ CS50 Python/Lecture_2/plates/plates.py
@@ -11,13 +11,13 @@ Implement a program that :
 - You're welcome to implement additional functions for "is_valid" to call (e.g., one function per requirement)
 
 Requirements : 
-- All vanity plates must start with at least two letters.
-- Vanity plates may contain a maximum of 6 characters (letters or numbers), and a minimum of 2 characters
-- Numbers cannot be used in the middle of a plate; they must come at the end. For example :
+1 - All vanity plates must start with at least two letters.
+2 - Vanity plates may contain a maximum of 6 characters (letters or numbers), and a minimum of 2 characters
+3 - Numbers cannot be used in the middle of a plate; they must come at the end. For example :
     - AAA222 would be an acceptable plate
     - AAA22A would not. 
-- The first number used cannot be a 0
-- No periods, spaces or punctuation marks are allowed
+4 - The first number used cannot be a 0
+5 - No periods, spaces or punctuation marks are allowed
 """
 
 def main() :
@@ -28,6 +28,23 @@ def main() :
         print("Invalid")
 
 def is_valid(s):
+    if start_letters(s) and min_max(s) :
+        return True
 
+def start_letters(s): # Requirement 1
+    if s[0].isalpha() and s[1].isalpha() :
+        return True
+
+def min_max(s): # Requirement 2
+    if 2 <= len(s) <= 6 :
+        return True
+
+# def numbers_place(s) : # Requirement 3
+
+# def first_number(s): # Requirement 4
+
+# def punctuation(s) : # Requirement 5
+     
+        
 
 main()
