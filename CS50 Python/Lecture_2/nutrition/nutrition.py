856816be947ecf8e8a254fa36640b085d8603233
allan.debiaggio <allan.de-biaggio@laplateforme.io>
2025-03-02 22:04:16 +0100

    Lecture 2 Exercise 5 wording Upload

---
+++ CS50 Python/Lecture_2/nutrition/nutrition.py
@@ -0,0 +1,9 @@
+"""
+Implement a program that :
+- Prompts users to input a fruit(case insensitively)
+- Outputs the number of calories in one portion of that fruit
+(per the FDA's poster for fruits, which is also available as text)
+- Capitalization aside, assume that users will input fruits
+exactly as written in the poster (e.g., "strawberries", not "strawberry")
+- Ignore any input that isn't a fruit
+"""
