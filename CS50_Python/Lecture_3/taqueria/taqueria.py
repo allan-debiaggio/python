f341935fc6e88807069559c2e87dd71a91b16dba
allan.debiaggio <allan.de-biaggio@laplateforme.io>
2025-03-27 16:49:49 +0100

    Lecture 3 Exercise 2

---
+++ CS50_Python/Lecture_3/taqueria/taqueria.py
@@ -0,0 +1,41 @@
+"""
+Implement a program that :
+- Enables user to place an order
+- Prompts them, one per line
+- Stops when the user enters Ctrl + D / CTRL D NOT WORKING ON WINDOWS APPARENTLY, USING CTRL C INSTEAD
+- After each inputted item, display the total prefixed with a dollar sign
+- Total formatted to two decimals
+- Input case insensitive
+- Ignore any input that is not in the dictionnary
+- Assume every item on the menu will be titlecased
+"""
+
+def main () :
+
+    items = {
+        "Baja Taco": 4.25,
+        "Burrito": 7.50,
+        "Bowl": 8.50,
+        "Nachos": 11.00,
+        "Quesadilla": 8.50,
+        "Super Burrito": 8.50,
+        "Super Quesadilla": 9.50,
+        "Taco": 3.00,
+        "Tortilla Salad": 8.00
+    }
+
+    total = 0
+
+    while True :
+        try :
+            order = input("Item: ")
+            order = order.lower()
+            order = order.title()
+            if order in items :
+                total += items[order]
+                print(f"${total:.2f}")
+            
+        except KeyboardInterrupt :
+            break
+
+main()
